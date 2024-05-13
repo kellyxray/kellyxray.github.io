28772e34d119665e61dd8598d122e212206371bc
kellyxray <166252810+kellyxray@users.noreply.github.com>
2024-05-13 00:44:08 -0400

    Update script.js
    testing Flickity carousel

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ function topNav() {
     x.className = "topnav";
   }
 }
+
+var elem = document.querySelector('.carousel');
+var flkty = new Flickity( elem, {
+    // options
+    cellAlign: 'center',
+    contain: true,
+    pageDots: false,
+    wrapAround: true,
+    autoPlay: true,
+    autoPlay: 1500,
+    selectedAttraction: 0.01,
+    friction: 0.15,
+    cellSelector: '.carousel-cell',
+    fullscreen: true,
+});
